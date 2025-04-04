3014298d9fe8aaee1620465a5dcaa754a4e1296e
desipossa <c1jang@hanmail.net>
2025-04-04 12:00:34 +0900

    20250404

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.main_visual_slide').slick({
+        autoplay: true,
+        speed: 600,
+        autoplaySpeed: 4000,
+        dots: true,
+        // arrows: false,
+    })
+})
