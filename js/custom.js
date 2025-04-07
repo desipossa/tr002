bd06c463ddad87b037232b362f849d474f19ccad
desipossa <c1jang@hanmail.net>
2025-04-07 14:25:54 +0900

    20250407

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,14 @@
 $(function () {
+    $('.main_visual .main_visual_slide').slick({
+        autoplay: true,
+        pauseOnHover: false,
+        arrows: false,
+    });
 
+    $('.main_visual .arrows .prev').on('click', function () {
+        $('.main_visual .main_visual_slide').slick('slickPrev')
+    });
+    $('.main_visual .arrows .next').on('click', function () {
+        $('.main_visual .main_visual_slide').slick('slickNext')
+    });
 })
