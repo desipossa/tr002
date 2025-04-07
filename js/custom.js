8d6eb51d200a9fcc89228a75704b6d0f00dd5b71
desipossa <c1jang@hanmail.net>
2025-04-07 12:08:38 +0900

    20250407

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,3 @@
 $(function () {
-    $('.main_visual_slide').slick({
-        autoplay: true,
-        speed: 600,
-        autoplaySpeed: 4000,
-        dots: true,
-        // arrows: false,
-    })
+
 })
