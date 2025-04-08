3d275eb62d3a20d67878f675dad03f3781ac4023
desipossa <c1jang@hanmail.net>
2025-04-08 11:00:27 +0900

    20250408

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,5 @@
 $(function () {
-    $('.main_visual .main_visual_slide').slick({
-        autoplay: true,
-        pauseOnHover: false,
+    $('.main_visual_slide').slick({
         arrows: false,
-    });
-
-    $('.main_visual .arrows .prev').on('click', function () {
-        $('.main_visual .main_visual_slide').slick('slickPrev')
-    });
-    $('.main_visual .arrows .next').on('click', function () {
-        $('.main_visual .main_visual_slide').slick('slickNext')
     });
 })
