7d8ce3550e374092e81e42c48d5c9307c693105e
desipossa <c1jang@hanmail.net>
2025-04-08 12:53:32 +0900

    20250408

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,19 @@
 $(function () {
     $('.main_visual_slide').slick({
         arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        autoplaySpeed: 6000,
+        speed: 1000,
     });
+
+
+    $('.main_visual .arrows .prev').on('click', function () {
+        $('.main_visual_slide').slick('slickPrev');
+    });
+
+    $('.main_visual .arrows .next').on('click', function () {
+        $('.main_visual_slide').slick('slickNext');
+    });
+
 })
