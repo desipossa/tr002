bf48c7efed6985d00bd5a92b5ce5d9eb615f25fd
desipossa <c1jang@hanmail.net>
2025-04-22 12:00:30 +0900

    20250422

--- 코지하우스따라만들기/js/custom.js
+++ 코지하우스따라만들기/js/custom.js
@@ -1,3 +1,13 @@
 $(function () {
+    const MMS = new Swiper('.main_menu_slide', {
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 80,
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+        },
 
+        speed: 5000,
+    })
 })
