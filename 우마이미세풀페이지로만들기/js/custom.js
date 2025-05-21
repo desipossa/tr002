213132db70ba162056a05df4ad102156657ca506
desipossa <c1jang@hanmail.net>
2025-05-21 12:47:34 +0900

    20250521

--- 우마이미세풀페이지로만들기/js/custom.js
+++ 우마이미세풀페이지로만들기/js/custom.js
@@ -11,17 +11,50 @@ $(function () {
         fixedElements: '#header , .side_lnk , .to_top',
         paddingTop: '80px',
         anchors: ['intro', 'store'],
+        css3: false,
 
         onLeave: function (o, d, dr) {
             if (d == 1) {
                 tl.restart();
             }
-            if (d == 2) {
+            if (d == 2 || d == 4) {
                 $('#header').addClass('on')
             } else {
                 $('#header').removeClass('on')
             }
         },
     });
+
+
+    const main_solution_slide = new Swiper('.main_solution_slide', {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 30,
+        loop: true,
+    });
+
+
+    $('#pomotion_movie').YTPlayer({
+        videoURL: 'https://youtu.be/neNsNfxl2cs',
+        containment: '#main_promotion',
+        showControls: false,
+        autoPlay: false,
+        playOnlyIfVisible: true,
+    });
+
+
+    let sw = true;
+
+    $('#main_promotion .cont button').on('click', function () {
+        $(this).toggleClass('on');
+
+        if (sw) {
+            $('#pomotion_movie').YTPPause();
+        } else {
+            $('#pomotion_movie').YTPPlay();
+        }
+
+        sw = !sw;
+    })
 
 });
