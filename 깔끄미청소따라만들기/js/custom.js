e8fe8db79a609862dc6fa5e5ec87485319c70194
desipossa <c1jang@hanmail.net>
2025-05-27 13:43:37 +0900

    20250527

---
+++ 깔끄미청소따라만들기/js/custom.js
@@ -0,0 +1,16 @@
+$(function () {
+    const main_slg_slide = new Swiper('.main_slg_slide', {
+        loop: true,
+        direction: 'vertical',
+        spaceBetween: 120,
+        speed: 1000,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            clickable: true,
+            el: '.main_visual .page',
+        },
+    })
+})
