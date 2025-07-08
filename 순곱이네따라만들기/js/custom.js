a0e5fe68aac12a1537941f9a3e57585402ef26a6
desipossa <c1jang@hanmail.net>
2025-07-08 12:46:28 +0900

    20250708

--- 순곱이네따라만들기/js/custom.js
+++ 순곱이네따라만들기/js/custom.js
@@ -22,5 +22,43 @@ $(function () {
             delay: 2500,
             disableOnInteraction: false,
         },
-    })
+    });
+
+
+    const main_solution_slide01 = new Swiper('.main_solution_slide01', {
+        loop: true,
+        direction: "vertical",
+        navigation: {
+            nextEl: "#main_solution .solution_wrap .arrows .next",
+            prevEl: "#main_solution .solution_wrap .arrows .prev",
+        },
+        pagination: {
+            el: "#main_solution .solution_wrap .page",
+            clickable: true,
+        },
+        on: {
+            slideChangeTransitionStart: function () {
+                $('#main_solution .solution_wrap .main_solution_slide01 .num').text('0' + (this.realIndex + 1))
+            }
+        }
+    });
+    const main_solution_slide02 = new Swiper('.main_solution_slide02', {
+        loop: true,
+        on: {
+            slideChangeTransitionStart: function () {
+                $('#main_solution .solution_wrap .main_solution_slide01 .num').text('0' + (this.realIndex + 1))
+            }
+        }
+    });
+    const main_solution_slide03 = new Swiper('.main_solution_slide03', {
+        loop: true,
+        allowTouchMove: false,
+    });
+
+
+    main_solution_slide01.controller.control = [main_solution_slide02, main_solution_slide03];
+    main_solution_slide02.controller.control = [main_solution_slide01, main_solution_slide03];
+
+
+
 })
